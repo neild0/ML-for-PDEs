b4dbda71ea34cc5ae5bdeb94e4154135e070b78d
Neil Deshmukh <neil.nitin.de@Gmail.com>
2020-08-01 22:28:17 -0400

    2D Data Generation Operational

--- DataGenerator/.ipynb_checkpoints/DataGene2D-checkpoint.py
+++ DataGenerator/.ipynb_checkpoints/DataGene2D-checkpoint.py
@@ -3,6 +3,7 @@ import random
 import os.path
 import pickle as pkl
 import torch
+from tqdm.auto import trange
 
 
 class DataGene2D:
@@ -34,21 +35,21 @@ class DataGene2D:
         
         self.x = np.append([np.arange(self.x_min,self.x_max,self.dx)],[self.x_max])
         self.x_range = self.x.shape[0]
-        self.x = np.expand_dims(self.x, axis=0)
-        self.x = np.repeat(self.x,IC,axis=0)
-        self.x = np.expand_dims(self.x,axis=2)
-        
+
         self.y = np.append([np.arange(self.y_min,self.y_max,self.dy)],[self.y_max])
         self.y_range = self.y.shape[0]
-        self.y = np.expand_dims(self.y, axis=0)
-        self.y = np.repeat(self.y,IC,axis=0)
-        self.y = np.expand_dims(self.y,axis=2)
+
+        self.data = np.array([(i,j) for i in self.x for j in self.y]).reshape(31,31,2)
+
+        self.data = np.expand_dims(self.data, axis=0)
+        self.data = np.repeat(self.data,self.IC,axis=0)
+        self.data = np.expand_dims(self.data,axis=4)
         
-        self.c = np.zeros((self.IC, n, m))
+        self.c = np.zeros((self.IC, self.n, self.m, 2))
         
         for seed in range(1,self.IC+1,1):
             np.random.seed(seed)
-            self.c[seed-1] = np.random.normal(0.0,1.0,self.n)
+            self.c[seed-1] = np.random.normal(0.0,1.0,(self.n,self.m,2))
             
     def DataSplit(self,u):
         allSamples_ = range(u.shape[0])
@@ -87,23 +88,25 @@ class DataGene2D:
     def Plain(self,dt):
         t = np.arange(0.0,self.T_max+dt,dt)
         t_range = t.shape[0]
-        t = np.expand_dims(t, axis=0)
+        t = np.expand_dims(t, axis=(0,1,2,3))
         t = np.repeat(t,self.IC,axis=0)
 
-        U = np.zeros((self.IC,self.x_range,self.y_range,t_range))
+        U = np.zeros((self.IC,self.x_range,self.y_range, 2, t_range))
 
-        t = np.expand_dims(t,axis=1)
         t = np.repeat(t,self.x_range,axis=1)
+        t = np.repeat(t,self.y_range,axis=2)
+        t = np.repeat(t,2,axis=3)
 
-        x = np.repeat(self.x,t_range,axis=2)
-
-        for i in range(self.n):
-            temp_c = \
-            np.repeat(np.repeat(np.expand_dims(np.expand_dims(self.c[:,i],axis=1),axis=1),self.x_range,axis=1),t_range,axis=2)
-            temp_exp = np.exp(-((i+1)**2)/self.beta*t)
-            temp_term = temp_c*np.sin((i+1)*x)
-            temp = temp_term*temp_exp
-            U += (1.0/self.n)*temp
+        self.data = np.repeat(self.data,t_range,axis=4)
+        
+        for i in trange(self.n):
+            for j in trange(self.m):
+                temp_c = np.expand_dims(self.c[:,i,j],axis=(1,2,4))
+                temp_c = np.repeat(np.repeat(np.repeat(temp_c,self.x_range,axis=1),self.y_range,axis=2), t_range, axis=4)
+                temp_exp = np.exp(t*-self.beta*((np.pi*i/self.x_max)**2)*(np.pi*j/self.y_max)**2)
+                temp_term = temp_c*np.sin((i*np.pi*self.data))
+                temp = temp_term*temp_exp
+                U += (1.0/self.n)*temp
             
         U_train, U_test = self.DataSplit(U)
             
